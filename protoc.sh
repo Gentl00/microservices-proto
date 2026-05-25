02dd9a6712c39354fff68c6861977f1a5519f582
Gentl00 <bienvenumakaba8@gmail.com>
2026-05-25 10:18:12 +0100

    proto update

--- protoc.sh
+++ protoc.sh
@@ -12,11 +12,12 @@ git config user.email "$EMAIL"
 git fetch --all && git checkout main
 
 sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
+echo "Installing Packages...."
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
-go install google.golang.org/grpc@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
-protoc --go_out=./golang --go_opt=paths=source_relative \
-  --go-grpc_out=./golang --go-grpc_opt=paths=source_relative \
+echo "Generating Go stubs"
+protoc --go_out=./golang/order --go_opt=paths=source_relative \
+  --go-grpc_out=./golang/order --go-grpc_opt=paths=source_relative \
  ./${SERVICE_NAME}/*.proto
 cd golang/${SERVICE_NAME}
 go mod init \
