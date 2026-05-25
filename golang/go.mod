f5dfa106efd7d8cfaa43495784a66afbcc0aefac
Gentl00 <bienvenumakaba8@gmail.com>
2026-05-25 10:30:38 +0100

    proto update

---
+++ golang/go.mod
@@ -0,0 +1,3 @@
+module github.com/Gentl00/microservices-proto/golang
+
+go 1.25.5
--- protoc.sh
+++ protoc.sh
@@ -5,7 +5,10 @@ RELEASE_VERSION=$2
 USER_NAME=$3
 EMAIL=$4
 
+set -e
+
 export PATH=$PATH:/usr/local/go/bin
+export PATH="$PATH:$HOME/go/bin"
 
 git config user.name "$USER_NAME"
 git config user.email "$EMAIL"
@@ -16,8 +19,8 @@ echo "Installing Packages...."
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 echo "Generating Go stubs"
-protoc --go_out=./golang/order --go_opt=paths=source_relative \
-  --go-grpc_out=./golang/order --go-grpc_opt=paths=source_relative \
+protoc --go_out=./golang --go_opt=paths=source_relative \
+  --go-grpc_out=./golang --go-grpc_opt=paths=source_relative \
  ./${SERVICE_NAME}/*.proto
 cd golang/${SERVICE_NAME}
 go mod init \
